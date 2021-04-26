fa7c3f2d33035258f960821d70f0a10267288d3c
Brendon J. Brewer <brendon.brewer@gmail.com>
2021-04-26 14:08:57 +1200

    Updated model concept

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CXX = g++
-FLAGS = -O2 -g -I include -Wall -Wextra -pedantic
+FLAGS = -std=c++20 -O2 -g -I . -I include -Wall -Wextra -pedantic
 
 default:
 	$(CXX) $(FLAGS) -c src/Example.cpp
+	$(CXX) $(FLAGS) -c include/Model.h
 	ar rcs libforklift.a *.o
-	rm -f *.o
+	rm -f *.o include/*.gch
 
---
+++ include/Model.h
@@ -0,0 +1,38 @@
+#ifndef Forklift_Model_h
+#define Forklift_Model_h
+
+#include <Tools/RNG.hpp>
+#include <vector>
+
+namespace Forklift
+{
+
+/*
+* Define the Model concept
+*/
+
+template<typename M>
+concept Model = requires(Tools::RNG& rng)
+{
+    { M{rng} };
+}
+&& requires(std::vector<unsigned char>&& bytes)
+{
+    { M(bytes) };
+}
+&& requires(const M& m)
+{
+    { m.scalars() } -> std::convertible_to<std::vector<double>>;
+    { m.bytes() } -> std::convertible_to<std::vector<unsigned char>>;
+}
+&& requires(M& m, Tools::RNG& rng)
+{
+    { m.perturb(rng) } -> std::convertible_to<double>;
+};
+
+
+
+} // namespace
+
+#endif
+
